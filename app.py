0ee9f32aa5817b32ae1da1b541f371ee5de976fe
trantruonghai <haitruongtran12112006@gmail.com>
2026-05-04 22:07:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,14 @@
-from flask import Flask, render_template, request
-from modelbook import Book
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-#Trang chủ
-@app.route('/')
+@app.route("/")
 def home():
-    books = Book.get_all_books()
-    return render_template('home.html', books=books)
+    books = [
+        {"id": 1, "title": "Python", "author": "Guido"},
+        {"id": 2, "title": "Flask", "author": "Armin"},
+    ]
+    return render_template("index.html", books=books)
 
-#Tìm kiếm
-@app.route('/search')
-def search():
-    keyword = request.args.get('keyword')
-    books = Book.search_books(keyword)
-    return render_template('home.html', books=books)
-
-#Thông tin sách 
-@app.route('/book/<int:book_id>')
-def book_detail(book_id):
-    book = Book.get_book_by_id(book_id)
-    return render_template('bookdetail.html', book=book)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
